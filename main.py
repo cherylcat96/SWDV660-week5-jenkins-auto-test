8e7cdf8d5f8f5dcdd2abf4b267490f3eec4443a1
cherylcat96 <97862727+cherylcat96@users.noreply.github.com>
2025-06-08 19:10:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("Hello from automated Jenkins job!")
 print("This build will be triggered by GitHub webhook")
-print("This file has been edited and should be triggered by GitHub webhook")
+print("This file has been edited and should be triggered by GitHub webhook!")
